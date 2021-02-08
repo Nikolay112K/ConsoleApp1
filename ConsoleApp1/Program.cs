d41d88a7890f5850654eead8de4cddf8471bf6fa
DESKTOP-QFTHJVS\Nik <kolya-kirich@i.ua>
2021-02-08 14:39:21 +0100

    New Project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ConsoleApp1.AccountSpace;
+using SomeStrangeClass;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(4);
+            Person tom = new Person { name = "Tom", age = 35 };
+        }
+    }
+
+    namespace AccountSpace
+    {
+        class Account
+        {
+            public int Id { get; private set; }
+            public Account(int _id)
+            {
+                Id = _id;
+            }
+        }
+    }
+}
